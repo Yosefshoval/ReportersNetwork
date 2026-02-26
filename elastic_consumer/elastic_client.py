cce782e19016b7a41bd2286728edb6368325c047
Yosef shoval <ys0542522504@gmail.com>
2026-02-26 15:27:44 +0200

    add upsert and search functions to elastic client

--- elastic_consumer/elastic_client.py
+++ elastic_consumer/elastic_client.py
@@ -1,4 +1,25 @@
+from elasticsearch import Elasticsearch
+from config import IndexerConfig
 
 class ElasticsearchClient:
     def __init__(self, logger: Logger):
         self.logger = logger
+        self.elastic_client = Elasticsearch(IndexerConfig.elastic_url)
+        self.index = IndexerConfig.elastic_index
+
+    def upsert(self, document: dict, image_id: str):
+        response = self.elastic_client.update(
+            index=self.index,
+            id=image_id,
+            doc=document,
+            doc_as_upsert=True
+        )
+        return response
+
+    def search(self, query: dict):
+        response = self.elastic_client.search(
+            index=self.index,
+            query=query
+        )
+
+        return response
